6159439805199dbf53755ae8cf8d4d13495b9809
SydMontague <sydmontague@phoenix-staffel.de>
2025-01-23 20:38:47 +0000

    Added devcontainer

--- src/compile.sh
+++ src/compile.sh
@@ -3,17 +3,17 @@
 cd "$(dirname "$0")"
 mkdir ../compiled/ -p
 
-FLAGS="-O3 -mabi=32 -march=r3000 -r -mel -nostdlib -mno-shared -fno-zero-initialized-in-bss -mno-gpopt -fno-inline-functions -fno-inline-small-functions -fno-exceptions -mno-check-zero-division -Wno-builtin-declaration-mismatch"
+FLAGS="-O3 -mabi=32 -march=r3000 -r -mel -nostdlib -mexplicit-relocs -mno-shared -fno-zero-initialized-in-bss -mno-gpopt -fno-inline-functions -fno-inline-small-functions -fno-exceptions -mno-check-zero-division -Wno-builtin-declaration-mismatch"
 
-mips-elf-g++ Utils.cpp UIElements.cpp -o ../compiled/utils.lib $FLAGS
-mips-elf-g++ Font.cpp Font5px.cpp Font7px.cpp -o ../compiled/font.lib $FLAGS
-mips-elf-g++ ItemEffects.cpp -o ../compiled/ItemEffects.lib $FLAGS
-mips-elf-g++ ItemFunctions.cpp -o ../compiled/ItemFunctions.lib $FLAGS
-mips-elf-g++ CustomUI.cpp -o ../compiled/CustomUI.lib $FLAGS
-mips-elf-g++ CustomInit.cpp -o ../compiled/CustomInit.lib $FLAGS
-mips-elf-g++ Evolution.cpp -o ../compiled/Evolution.lib $FLAGS
-mips-elf-g++ StatsView.cpp -o ../compiled/StatsView.lib $FLAGS
-mips-elf-g++ StatsViewData.cpp -o ../compiled/StatsViewData.lib $FLAGS
-mips-elf-g++ Partner.cpp -o ../compiled/Partner.lib $FLAGS
+mips-g++ Utils.cpp UIElements.cpp -o ../compiled/utils.lib $FLAGS
+mips-g++ Font.cpp Font5px.cpp Font7px.cpp -o ../compiled/font.lib $FLAGS
+mips-g++ ItemEffects.cpp -o ../compiled/ItemEffects.lib $FLAGS
+mips-g++ ItemFunctions.cpp -o ../compiled/ItemFunctions.lib $FLAGS
+mips-g++ CustomUI.cpp -o ../compiled/CustomUI.lib $FLAGS
+mips-g++ CustomInit.cpp -o ../compiled/CustomInit.lib $FLAGS
+mips-g++ Evolution.cpp -o ../compiled/Evolution.lib $FLAGS
+mips-g++ StatsView.cpp -o ../compiled/StatsView.lib $FLAGS
+mips-g++ StatsViewData.cpp -o ../compiled/StatsViewData.lib $FLAGS
+mips-g++ Partner.cpp -o ../compiled/Partner.lib $FLAGS
 
 cd -
